dbe6c2cf3d1b7a8be58cf29590eede0ce153ce3c
Cleidson Goes <102970710+CleidsonGoes@users.noreply.github.com>
2023-04-25 22:28:12 -0300

    Add files via upload

---
+++ Dia_01:_HTML_e_CSS_-_Forms/treinando_preventDefault/script.js
@@ -0,0 +1,21 @@
+const INPUT_TEXT = document.querySelector("#input-text");
+const INPUT_CHECKBOX = document.querySelector("#input-checkbox");
+const HREF_LINK = document.querySelector("#href");
+
+HREF_LINK.addEventListener('click',(event)=>{
+    event.preventDefault();
+});
+
+INPUT_CHECKBOX.addEventListener('click',(event)=>{
+    event.preventDefault();
+});
+
+//Utilizando o event.key, é possível capturar qual tecla
+//foi pressionada e então submeter o caractere correspondente
+//a uma condição.
+INPUT_TEXT.addEventListener('keypress',(event)=>{
+    const character = event.key;
+    if (character != "a") {
+        event.preventDefault();
+    }
+})
